cb4a35096f48431de206cffa09d37968a7a56100
yukimemi <yukimemi@gmail.com>
2014-04-16 21:12:33 +0900

    Update README.md

--- README.md
+++ README.md
@@ -2,3 +2,151 @@ md2docx
 =======
 
 convert markdown to docx
+
+markdownファイルをwordのdocxに変換するスクリプトです。
+* PowerShellスクリプトなのでWindows限定です。
+* 動作確認はOffice 2010でのみ行っています。
+* 確認出来ているのは、Windows 7 + PowerShell V3です。
+
+使い方
+------
+
+バッチファイル化してあるので、そちらを使用します。
+コマンドプロンプトより、以下のように入力します。
+
+```Batchfile
+> md2docx.cmd file.md
+```
+
+拡張子が **docx** に変更された、 **file.docx** が同じディレクトリに出来ているはずです。
+
+もしくは、引数に何も指定しなければ、markdown file の入力を促すプロンプトが表示されるので、そこにエクスプローラなどから markdownファイルをドラッグアンドドロップしてEnterでもよいです。
+
+対応記述形式
+------------
+
+markdownといっても、現在対応している記述はまだまだ少ないです。
+また、word独自対応するためにmarkdownにおけるコメントを使用している部分があります。
+
+### 見出し
+
+```Markdown
+# 見出し 1
+## 見出し 2
+### 見出し 3
+#### 見出し 4
+```
+
+それぞれ、wordのスタイル [見出し 1] 〜 [見出し 4]に対応しています。
+
+### 表題、副題
+
+```Markdown
+#<!--!title--> 表題
+#<!--!subtitle--> 副題
+```
+
+それぞれ、wordのスタイル [表題]、[副題]に対応しています。
+
+### リスト
+
+```Markdown
+* リスト 1
+* リスト 2
+* リスト 3
+
+1. リスト 1
+2. リスト 2
+3. リスト 3
+```
+
+通常のリストと、番号付きリストです。
+
+### 画像
+
+```Markdown
+![代替テキスト](imgfolder/img.jpg)
+![代替テキスト](imgfolder/img.jpg)<!--!200x100-->
+![代替テキスト](imgfolder/img.jpg)<!--!300x-->
+![代替テキスト](imgfolder/img.jpg)<!--!x200-->
+```
+
+画像の場所は、markdownファイルのある場所からの相対パスで記述します。
+また、後ろに上記のように画像のサイズを指定することが可能です。
+画像のサイズは縦横比固定なので、両方指定しても恐らくどちらか無視されます。
+なので、片方だけ指定することも可能です。
+
+### 改ページ
+
+```Markdown
+<!--![改ページ]-->
+<!--![PageBreak]-->
+```
+
+改ページを挿入します。
+
+### インラインマクロ
+
+```Markdown
+右寄せ文章<!--!$selection.ParagraphFormat.Alignment = $CONST.wdAlignParagraphRight-->
+```
+
+`<!--! word マクロ -->` でwordマクロ(PowerShell記述)が実行出来ます。
+
+該当文章のみにマクロを指定したい場合に便利です。
+この場合、「右寄せ文章」が右寄せになります。
+
+提供されるwordのCOMオブジェクトは、以下の通りです。
+
+```Markdown
+$word      : Word オブジェクト
+$doc       : Word Document オブジェクト
+$selection : Word Selection オブジェクト
+```
+
+また、wordで使用出来る定数は、`$CONST.定数名`でアクセス出来ます。(まだアクセス出来る定数は少ない・・・)
+
+### 複数行マクロ
+
+```PowerShell
+<!--!
+  # Style change
+  $doc.Styles.Item("表題").Font.NameFarEast = "ＭＳ Ｐ明朝"
+  $doc.Styles.Item("表題").Font.Size = 20
+  $doc.Styles.Item("表題").Font.Bold = $true
+
+  $doc.Styles.Item("見出し 1").Font.NameFarEast = "ＭＳ Ｐ明朝"
+  $doc.Styles.Item("見出し 1").Font.Size = 16
+  $doc.Styles.Item("見出し 1").Font.Bold = $true
+  $doc.Styles.Item("見出し 1").AutomaticallyUpdate = $true
+
+  # Page setup
+  $doc.PageSetup.TopMargin = $word.MillimetersToPoints(25)
+  $doc.PageSetup.BottomMargin = $word.MillimetersToPoints(20)
+  $doc.PageSetup.LeftMargin = $word.MillimetersToPoints(20)
+  $doc.PageSetup.RightMargin = $word.MillimetersToPoints(15)
+
+  # Image size change
+  $doc.InlineShapes | % {
+    if ($_.Height -gt 200 ) {
+      $_.LockAspectRatio = $CONST.msoTrue; $_.Height = 200
+    }
+  }
+-->
+```
+
+`<!--!`で始めて、`-->`で終わる、この記述内はすべてPowerShellとして実行されます。
+インラインマクロと同様、`$word`、`$doc`、`$selection`が使用出来ます。
+
+`<!--!`と、`-->`はそれぞれ、単独で行頭に記述されている必要があります。
+
+このように、結局書式の設定部分は、wordマクロをガリガリ書くことになります・・・。
+その代わり、wordマクロを(PowerShellにて)記述出来るので、その気になればなんでも出来る！はずです。
+
+TODO
+----
+
+* テーブル
+* 引用
+* 処理速度アップ
+
---
+++ md2docx.cmd
@@ -0,0 +1,237 @@
+@echo off
+pushd "%~dp0" > nul
+set tm=%time: =0%
+set ps1file=%~n0___%date:~-10,4%%date:~-5,2%%date:~-2,2%_%tm:~0,2%%tm:~3,2%%tm:~6,2%%tm:~9,2%.ps1
+for /f "usebackq skip=10 delims=" %%i in ("%~f0") do @echo %%i >> "%ps1file%"
+powershell -NoProfile -ExecutionPolicy unrestricted -File "%ps1file%" %*
+del "%ps1file%"
+popd > nul
+pause
+exit /b %ERRORLEVEL%
+# ========== do ps1 file as a dosbatch ==========
+# markdown -> docx
+param($mdFile = $(Read-Host "Enter markdown file path"))
+
+$ErrorActionPreference = "stop"
+$DebugPreference = "Continue" # Continue SilentlyContinue Stop Inquire
+# for break point use $host.EnterNestedPrompt()
+
+$commandPath = Split-Path -parent $myInvocation.MyCommand.path
+$commandName = Split-Path -leaf $myInvocation.MyCommand.path
+$commandBaseName = ((gci $myInvocation.MyCommand.path).BaseName -split "___")[0]
+
+Set-Location $commandPath
+
+<# for test
+$mdFile = Join-Path $commandPath "test.md"
+#>
+
+# Const variable
+$CONST = @{
+  wdOutlineNumberGallery = 3
+  wdListApplyToWholeList = 0
+  msoFalse = 0
+  msoTrue = -1
+  wdAlignParagraphRight = 2
+  wdSeekMainDocument = 0
+  wdSeekCurrentPageHeader = 9
+  wdSeekCurrentPageFooter = 10
+  wdFieldEmpty = -1
+  wdFieldPage = 33
+}
+
+function checkFilePath($path) {#{{{
+
+  trap { Write-Host "[checkFilePath]: Error $($_)"; throw $_ }
+
+  if (! (Test-Path $path)) {
+    Write-Host "$($path) is not found !"
+    return $false
+  }
+
+  $path = $path -replace """", ""
+
+  # to abs path
+  $path = Convert-Path $path
+
+  Write-Debug $path
+
+  return $path
+
+}#}}}
+
+function typeText($line, $word, $doc, $selection, [ref]$commandFlg) {#{{{
+
+  trap { Write-Host "[typeText]: Error $($_)"; throw $_ }
+
+  # command
+  if ($commandFlg.Value) {
+    # Check command end
+    if ($line -match "^-->$") {
+      #Write-Debug "---------- command end ----------"
+      # do command
+      $commandStr = $commandList.ToArray() -join "`n"
+      #Write-Debug $commandStr
+      Invoke-Expression $commandStr
+      $commandList = New-Object System.Collections.Generic.List[string]
+      $commandFlg.Value = $false
+      return
+    }
+    $commandList.Add($line)
+    return
+  }
+
+  switch -regex ($line) {
+    # title
+    "^#<!--!title--> " {
+            $line = $line -replace "^#<!--!title--> ", ""
+            $selection.TypeText($line)
+            $selection.Style = $doc.Styles.Item("表題")
+            $selection.TypeParagraph()
+          }
+    # subtitle
+    "^#<!--!subtitle--> " {
+            $line = $line -replace "^#<!--!subtitle--> ", ""
+            $selection.TypeText($line)
+            $selection.Style = $doc.Styles.Item("副題")
+            $selection.TypeParagraph()
+          }
+    # head 1
+    "^# " {
+            $line = $line -replace "^# ", ""
+            $selection.TypeText($line)
+            $selection.Style = $doc.Styles.Item("見出し 1")
+            $selection.TypeParagraph()
+          }
+    # head 2
+    "^## " {
+            $line = $line -replace "^## ", ""
+            $selection.TypeText($line)
+            $selection.Style = $doc.Styles.Item("見出し 2")
+            $selection.TypeParagraph()
+          }
+    # head 3
+    "^### " {
+            $line = $line -replace "^### ", ""
+            $selection.TypeText($line)
+            $selection.Style = $doc.Styles.Item("見出し 3")
+            $selection.TypeParagraph()
+          }
+    # head 4
+    "^#### " {
+            $line = $line -replace "^#### ", ""
+            $selection.TypeText($line)
+            $selection.Style = $doc.Styles.Item("見出し 4")
+            $selection.TypeParagraph()
+          }
+    # bullet list
+    "^\* " {
+            $line = $line -replace "^\* ", ""
+            $selection.Range.ListFormat.ApplyBulletDefault()
+            $selection.TypeText($line)
+            $selection.TypeParagraph()
+            $selection.Style = $doc.Styles.Item("標準")
+          }
+    # number list
+    "^[0-9]+\. " {
+            $line = $line -replace "^[0-9]+\. ", ""
+            $selection.Range.ListFormat.ApplyNumberDefault()
+            $selection.TypeText($line)
+            $selection.TypeParagraph()
+            $selection.Style = $doc.Styles.Item("標準")
+          }
+    # image
+    "!\[(?<imgName>.*)\]\((?<imgUrl>.*)\)(<!--!(?<width>[0-9]*)x(?<height>[0-9]*)-->|.*)" {
+            $imgPath = $(Join-Path (Split-Path -parent $mdFile) $matches.imgUrl)
+            if (Test-Path $imgPath) {
+              $img = $selection.InlineShapes.AddPicture($imgPath)
+              #Write-Debug "Height: [$($img.Height)] Width: [$($img.Width)]"
+              $img.LockAspectRatio = $CONST.msoTrue
+              if ($matches.width) {
+                $img.Width = $matches.width
+              }
+              if ($matches.height) {
+                $img.Height = $matches.height
+              }
+            } else {
+              Write-Error "$($imgPath) is not found !"
+            }
+            $selection.TypeParagraph()
+          }
+    # page break
+    "^<!--!(\[改ページ\]|\[PageBreak\])-->" {
+            $selection.InsertBreak()
+            break
+          }
+    # comment command start
+    "^<!--!$" {
+            #Write-Debug "---------- command start ----------"
+            $commandFlg.Value = $true
+          }
+    # comment
+    "<!--[^!].*-->" {
+            # do nothing
+            #Write-Debug "comment"
+          }
+    # other
+    default {
+            $selection.Style = $doc.Styles.Item("標準")
+            # comment command
+            if ($line -match "<!--!(?<command>.*)-->") {
+              $selection.TypeText($($line -replace "<!--!.*-->", ""))
+              Write-Debug $matches.command
+              Invoke-Expression $matches.command
+            } else {
+              $selection.TypeText($line)
+            }
+            $selection.TypeParagraph()
+          }
+  }
+
+}#}}}
+
+function main() {#{{{
+
+  trap { Write-Host "[main]: Error $($_)"; throw $_ }
+
+  $commandList = New-Object System.Collections.Generic.List[string]
+  $commandFlg = $false
+
+  # check file
+  $mdFile = checkFilePath $mdFile
+  if (! $mdFile) {
+    return -1
+  }
+
+  Write-Host "Executing..."
+
+  # Open docx
+  try {
+
+    $word = New-Object -ComObject Word.Application
+    #$word.Visible = $true
+
+    $doc = $word.Documents.Add()
+    $selection = $word.Selection
+
+    gc -Encoding UTF8 $mdFile | % {
+      #Write-Debug $_
+      typeText $_ $word $doc $selection ([ref]$commandFlg)
+    }
+
+    $mdFileInfo = gci $mdFile
+    $doc.SaveAs([ref]$(Join-Path $mdFileInfo.DirectoryName ($mdFileInfo.BaseName + ".docx")))
+
+  } catch [Exception] {
+    throw "[main]: Error $($_)"
+  } finally {
+    if ($word) {
+      $word.Quit()
+    }
+  }
+
+} #}}}
+
+# call main
+Measure-Command { main }
+
