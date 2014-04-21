5383ff2f13f1effe0ff9f5af6e1c8c36d61e2b4c
yukimemi <yukimemi@gmail.com>
2014-04-21 18:13:49 +0900

    add support for table

--- md2docx.cmd
+++ md2docx.cmd
@@ -7,13 +7,13 @@ powershell -NoProfile -ExecutionPolicy unrestricted -File "%ps1file%" %*
 del "%ps1file%"
 popd > nul
 pause
-exit /b %ERRORLEVEL%
+exit %ERRORLEVEL%
 # ========== do ps1 file as a dosbatch ==========
 # markdown -> docx
 param($mdFile = $(Read-Host "Enter markdown file path"))
 
 $ErrorActionPreference = "stop"
-$DebugPreference = "SilentlyContinue" # Continue SilentlyContinue Stop Inquire
+$DebugPreference = "Continue" # Continue SilentlyContinue Stop Inquire
 # for break point use $host.EnterNestedPrompt()
 
 $commandPath = Split-Path -parent $myInvocation.MyCommand.path
@@ -27,48 +27,112 @@ $mdFile = Join-Path $commandPath "test.md"
 #>
 
 # Const variable
-$CONST = @{
+$CONST = @{#{{{
   msoConnectorCurve = 3
   msoConnectorElbow = 2
   msoConnectorStraight = 1
   msoConnectorTypeMixed = -2
   msoFalse = 0
-  msoTrue = -1
-  wdAlignParagraphCenter = 1
-  wdAlignParagraphLeft = 0
-  wdAlignParagraphRight = 2
-  wdFieldEmpty = -1
-  wdFieldPage = 33
-  wdHeaderFooterEvenPages = 3
-  wdHeaderFooterFirstPage = 2
-  wdHeaderFooterPrimary = 1
-  wdListApplyToWholeList = 0
-  wdOutlineNumberGallery = 3
-  wdSeekCurrentPageFooter = 10
-  wdSeekCurrentPageHeader = 9
-  wdSeekMainDocument = 0
-  wdThemeColorAccent1 = 4
-  wdThemeColorAccent2 = 5
-  wdThemeColorAccent3 = 6
-  wdThemeColorAccent4 = 7
-  wdThemeColorAccent5 = 8
-  wdThemeColorAccent6 = 9
   msoLineSingle = 1
   msoLineStyleMixed = -2
   msoLineThickBetweenThin = 5
   msoLineThickThin = 4
   msoLineThinThick = 3
   msoLineThinThin = 2
-}
+  msoTrue = -1
+  wdAlertsAll = -1
+  wdAlertsMessageBox = -2
+  wdAlertsNone = 0
+  wdAlignParagraphCenter = 1
+  wdAlignParagraphLeft = 0
+  wdAlignParagraphRight = 2
+  wdAutoFitContent = 1
+  wdAutoFitFixed = 0
+  wdAutoFitWindow = 2
+  wdColumnBreak = 8
+  wdDoNotSaveChanges = 0
+  wdFieldEmpty = -1
+  wdFieldPage = 33
+  wdHeaderFooterEvenPages = 3
+  wdHeaderFooterFirstPage = 2
+  wdHeaderFooterPrimary = 1
+  wdLineBreak = 6
+  wdLineBreakClearLeft = 9
+  wdLineBreakClearRight = 10
+  wdListApplyToWholeList = 0
+  wdOutlineNumberGallery = 3
+  wdPageBreak = 7
+  wdPromptToSaveChanges = -2
+  wdSaveChanges = -1
+  wdSectionBreakContinuous = 3
+  wdSectionBreakEvenPage = 4
+  wdSectionBreakNextPage = 2
+  wdSectionBreakOddPage = 5
+  wdSeekCurrentPageFooter = 10
+  wdSeekCurrentPageHeader = 9
+  wdSeekMainDocument = 0
+  wdTextWrappingBreak = 11
+  wdThemeColorAccent1 = 4
+  wdThemeColorAccent2 = 5
+  wdThemeColorAccent3 = 6
+  wdThemeColorAccent4 = 7
+  wdThemeColorAccent5 = 8
+  wdThemeColorAccent6 = 9
+  wdWord8TableBehavior = 0
+  wdWord9TableBehavior = 1
+  wdTableFormat3DEffects1 = 32
+  wdTableFormat3DEffects2 = 33
+  wdTableFormat3DEffects3 = 34
+  wdTableFormatClassic1 = 4
+  wdTableFormatClassic2 = 5
+  wdTableFormatClassic3 = 6
+  wdTableFormatClassic4 = 7
+  wdTableFormatColorful1 = 8
+  wdTableFormatColorful2 = 9
+  wdTableFormatColorful3 = 10
+  wdTableFormatColumns1 = 11
+  wdTableFormatColumns2 = 12
+  wdTableFormatColumns3 = 13
+  wdTableFormatColumns4 = 14
+  wdTableFormatColumns5 = 15
+  wdTableFormatContemporary = 35
+  wdTableFormatElegant = 36
+  wdTableFormatGrid1 = 16
+  wdTableFormatGrid2 = 17
+  wdTableFormatGrid3 = 18
+  wdTableFormatGrid4 = 19
+  wdTableFormatGrid5 = 20
+  wdTableFormatGrid6 = 21
+  wdTableFormatGrid7 = 22
+  wdTableFormatGrid8 = 23
+  wdTableFormatList1 = 24
+  wdTableFormatList2 = 25
+  wdTableFormatList3 = 26
+  wdTableFormatList4 = 27
+  wdTableFormatList5 = 28
+  wdTableFormatList6 = 29
+  wdTableFormatList7 = 30
+  wdTableFormatList8 = 31
+  wdTableFormatNone = 0
+  wdTableFormatProfessional = 37
+  wdTableFormatSimple1 = 1
+  wdTableFormatSimple2 = 2
+  wdTableFormatSimple3 = 3
+  wdTableFormatSubtle1 = 38
+  wdTableFormatSubtle2 = 39
+  wdTableFormatWeb1 = 40
+  wdTableFormatWeb2 = 41
+  wdTableFormatWeb3 = 42
+}#}}}
 
 function rgb($r, $g, $b) {#{{{
   return ($b + ($g * 256) + ($r * 65536))
 }#}}}
 
 function randomColor() {#{{{
-  $r = $(1..255 | Get-Random)
-  $g = $(1..255 | Get-Random)
-  $b = $(1..255 | Get-Random)
+  $r = $(0..255 | Get-Random)
+  $g = $(0..255 | Get-Random)
+  $b = $(0..255 | Get-Random)
   return rgb $r $g $b
 }#}}}
 
@@ -92,7 +156,7 @@ function checkFilePath($path) {#{{{
 
 }#}}}
 
-function typeText($line, $word, $doc, $selection, [ref]$commandFlg) {#{{{
+function typeText($line, $word, $doc, $selection, [ref]$commandFlg, [ref]$tableMap) {#{{{
 
   trap { Write-Host "[typeText]: Error $($_)"; throw $_ }
 
@@ -111,6 +175,25 @@ function typeText($line, $word, $doc, $selection, [ref]$commandFlg) {#{{{
     }
     $commandList.Add($line)
     return
+  }
+
+  # table
+  if ($tableMap.Value.flg) {
+    # Check tale end
+    if ($line -notmatch "^\|.*\|$") {
+      Write-Debug "---------- table end ----------"
+      Write-Debug "table row = $($tableMap.Value.row)"
+      Write-Debug "table col = $($tableMap.Value.col)"
+
+      $table = $doc.Range([ref]$tableMap.Value.rangeStart, [ref]$tableMap.Value.rangeEnd).ConvertToTable("|", $tableMap.Value.row, $tableMap.Value.col)
+      $table.AutoFormat($CONST.wdTableFormatElegant)
+
+      $tableMap.Value.row = 0
+      $tableMap.Value.col = 0
+      $tableMap.Value.rangeStart = 0
+      $tableMap.Value.rangeEnd = 0
+      $tableMap.Value.flg = $false
+    }
   }
 
   switch -regex ($line) {
@@ -195,10 +278,31 @@ function typeText($line, $word, $doc, $selection, [ref]$commandFlg) {#{{{
             $selection.InsertBreak()
             break
           }
+    # section break
+    "^<!--!(\[‰üƒZƒNƒVƒ‡ƒ“\]|\[SectionBreak\])-->" {
+            $selection.InsertBreak($CONST.wdSectionBreakNextPage)
+            break
+          }
     # comment command start
     "^<!--!$" {
             #Write-Debug "---------- command start ----------"
             $commandFlg.Value = $true
+          }
+    # table
+    "^\|.*\|$" {
+            Write-Debug "---------- table start ----------"
+            $lineArray = $line.SubString(1, $line.Length -2) -split "\|"
+            $tableMap.Value.col = $lineArray.Length
+            $tableMap.Value.row++
+
+            if (! $tableMap.Value.flg) {
+              $tableMap.Value.rangeStart = $selection.Start
+            }
+
+            $selection.TypeText($line.SubString(1, $line.Length -2))
+            $selection.TypeParagraph()
+            $tableMap.Value.rangeEnd = $selection.End
+            $tableMap.Value.flg = $true
           }
     # comment
     "<!--[^!].*-->" {
@@ -228,6 +332,13 @@ function main() {#{{{
 
   $commandList = New-Object System.Collections.Generic.List[string]
   $commandFlg = $false
+  $tableMap = @{
+    flg = $false
+    row = 0
+    col = 0
+    rangeStart = 0
+    rangeEnd = 0
+  }
 
   # check file
   $mdFile = checkFilePath $mdFile
@@ -241,14 +352,15 @@ function main() {#{{{
   try {
 
     $word = New-Object -ComObject Word.Application
-    #$word.Visible = $true
+    $word.Application.DisplayAlerts = $CONST.wdAlertsNone
+    $word.Visible = $true
 
     $doc = $word.Documents.Add()
     $selection = $word.Selection
 
     gc -Encoding UTF8 $mdFile | % {
       #Write-Debug $_
-      typeText $_ $word $doc $selection ([ref]$commandFlg)
+      typeText $_ $word $doc $selection ([ref]$commandFlg) ([ref]$tableMap)
     }
 
     $mdFileInfo = gci $mdFile
@@ -258,7 +370,7 @@ function main() {#{{{
     throw "[main]: Error $($_)"
   } finally {
     if ($word) {
-      $word.Quit()
+      $word.Quit([ref]$CONST.wdDoNotSaveChanges)
     }
   }
 
